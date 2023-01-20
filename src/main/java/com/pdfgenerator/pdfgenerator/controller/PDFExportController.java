5073c150d92ff847e4b445a2377a2f3620c22dc1
Leonid Barsucovschi <leonidbarsdev1993@gmail.com>
2023-01-20 11:27:17 +0200

    create service and controller for pdf export

---
+++ src/main/java/com/pdfgenerator/pdfgenerator/controller/PDFExportController.java
@@ -0,0 +1,36 @@
+package com.pdfgenerator.pdfgenerator.controller;
+
+import com.pdfgenerator.pdfgenerator.service.PDFGeneratorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author leonid.barsucovschi
+ */
+
+@Controller
+@RequiredArgsConstructor
+public class PDFExportController {
+
+    private final PDFGeneratorService pdfGeneratorService;
+
+    @GetMapping("/pdf/generate")
+    public void generatePDF(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
+        String currentDateTime = dateFormat.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        this.pdfGeneratorService.export(response);
+    }
+}
